1718c05179934527d76ed16d2c7112494832e3fa
afiab <abidica30@stuy.edu>
2024-03-16 02:36:00 -0400

    added shuffle cards feature

--- flashcards2/src/App.jsx
+++ flashcards2/src/App.jsx
@@ -6,15 +6,29 @@ import Flashcard from './component/Flashcard'
 import Button from './component/Button'
 import French from './component/french.json'
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 function App() {
   const [index, setIndex] = useState(0);
-  // const [card, setCard] = useState(French[0]);
+  const [card, setCards] = useState(French);
 
   function nextCard() {
     setIndex(nextIndex => nextIndex+1)
   }
   function prevCard() {
     setIndex(nextIndex => nextIndex-1)
+  }
+
+  function shuffleCards() {
+    const shuffledCards = shuffle([...card]);
+    setCards(shuffledCards);
+    setIndex(0);
   }
 
   return (
@@ -25,11 +39,11 @@ function App() {
         <h2>Number of cards: {French.length}</h2>
       </div>
       <Flashcard 
-        {...French[index]}
+        {...card[index]}
       />
       <Button onClick={prevCard} disabled = {index === 0 ? true:false}>&larr;</Button>
       <Button onClick={nextCard} disabled = {index === French.length - 1 ? true:false}>&rarr;</Button>
-      <Button>Shuffle Cards</Button>
+      <Button onClick={shuffleCards}>Shuffle Cards</Button>
     </div>    
   )
 }
